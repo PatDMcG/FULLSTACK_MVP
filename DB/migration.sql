359b63430ca048bba5fb33d0fb166f8cb3f03d1f
PatDMcG <110838464+PatDMcG@users.noreply.github.com>
2023-06-16 16:53:58 -0400

    fleshed out DB SQL files
    tested locally

---
+++ DB/migration.sql
@@ -0,0 +1,6 @@
+drop table if exists chores;
+create table chores(
+    Title varchar,
+    Est_Time_min int,
+    Id serial
+);
---
+++ DB/seed.sql
@@ -0,0 +1,4 @@
+insert into chores (Title,Est_Time_min) values ('walk the dog', 90);
+insert into chores (Title,Est_Time_min) values ('study', 120);
+insert into chores (Title,Est_Time_min) values ('make bed', 15);
+insert into chores (Title,Est_Time_min) values ('defalt test', 0);
