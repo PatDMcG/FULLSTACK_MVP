a348ee7314b31da0591cfa7126c7df5dd613f7ab
PatDMcG <110838464+PatDMcG@users.noreply.github.com>
2023-06-18 17:05:47 -0400

    serving non index.html

--- API/main.js
+++ API/main.js
@@ -6,7 +6,7 @@ const path = require('path')
 dotenv.config()
 const app = express()
 app.use(express.json())
-app.use('/', express.static(path.join(__dirname, '../Front_END')))
+app.get('/', (req,res) => {res.sendFile(path.join(__dirname, '../Front_END/main.html'))})
 const PORT = process.env.PORT
 const connectionString = process.env.DATABASE_URL
 const pool = new Pool({
