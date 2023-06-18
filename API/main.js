364dd3e129bfd352449a18fba13ce38cb4ae04d8
PatDMcG <110838464+PatDMcG@users.noreply.github.com>
2023-06-18 16:53:57 -0400

    working on static page

--- API/main.js
+++ API/main.js
@@ -6,7 +6,7 @@ const path = require('path')
 dotenv.config()
 const app = express()
 app.use(express.json())
-app.use(express.static(path.join(__dirname, '../Front_END')))
+app.use('/', express.static(path.join(__dirname, '../Front_END')))
 const PORT = process.env.PORT
 const connectionString = process.env.DATABASE_URL
 const pool = new Pool({
