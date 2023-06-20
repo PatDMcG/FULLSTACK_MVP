20f8bfb2f3958cc1737fc9be3ff25db0a04dab63
PatDMcG <110838464+PatDMcG@users.noreply.github.com>
2023-06-20 09:57:44 -0400

    and  again

--- Front_END/main.js
+++ Front_END/main.js
@@ -5,12 +5,16 @@ async function grabber(input) {
 
     const response = await fetch(`${input}`);
       const data = await response.json();
-      console.log(data)
-    return data
       
-    
-    };
-    chores = document.createElement("p1")
-    chores.innerText = grabber(`${API}chores`)
+    for(i=0;i<data.length;i++)
+    {
+        chores = document.createElement("p1")
+    chores.innerText = data[i]
     document.getElementById("chores").appendChild(chores)
     console.log(chores)
+    }
+    
+    };
+grabber(`${API}chores`)
+
+    
