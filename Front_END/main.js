c29026d9a3e1aba21a5079d2378773858a02fd76
PatDMcG <110838464+PatDMcG@users.noreply.github.com>
2023-06-20 10:46:49 -0400

    Working Data retrival

--- Front_END/main.js
+++ Front_END/main.js
@@ -5,16 +5,35 @@ async function grabber(input) {
 
     const response = await fetch(`${input}`);
       const data = await response.json();
-      
-    for(i=0;i<data.length;i++)
-    {
-        chores = document.createElement("p1")
-    chores.innerText = data[i]
-    document.getElementById("chores").appendChild(chores)
-    console.log(chores)
-    }
+    displayer(data)
     
     };
 grabber(`${API}chores`)
 
-    
+    function displayer(data)
+    {
+        for(i=0;i<data.length;i++)
+    {
+        e=i+1
+        tile = document.createElement("div")
+        titleTile = document.createElement("div")
+        timeTile = document.createElement("div")
+        tile.id = `${e}`
+        title = document.createElement("p1")
+        time = document.createElement("p1")
+        delet = document.createElement("button")
+        editTitle = document.createElement("button")
+        editTime = document.createElement("button")
+        titleTile.appendChild(editTitle)
+        timeTile.appendChild(editTime)
+        title.innerText = data[i].title
+        time.innerText = data[i].est_time_min
+        console.log(time)
+        titleTile.appendChild(title)
+        timeTile.appendChild(time)
+        tile.appendChild(titleTile)
+        tile.appendChild(timeTile)
+        tile.appendChild(delet)
+        document.getElementById("chores").appendChild(tile)
+    }
+    }
