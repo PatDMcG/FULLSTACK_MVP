c7bc913ac5d82d337d117a937d0ae7f32869de54
PatDMcG <110838464+PatDMcG@users.noreply.github.com>
2023-06-20 17:21:22 -0400

    again debug

--- API/main.js
+++ API/main.js
@@ -63,7 +63,7 @@ app.get('/chores/:id', async (req, res) => {
     if(Est_Time_min !== null){await pool.query("update chores set Title = $1 where id = $2", [Est_Time_min,id])}
     let data = await pool.query("select * from chores where id = $1", [id]) 
     console.log(data.rows)
-     res.status(200).send(data.rows)
+     res.status(200).send(req.body)
               
 } catch (error) {
     res.status(500).send(error)
