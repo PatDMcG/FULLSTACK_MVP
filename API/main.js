ae91608e0ae3dd29d402ba571ec5d574cefb475f
PatDMcG <110838464+PatDMcG@users.noreply.github.com>
2023-06-20 17:33:45 -0400

    lowercases?

--- API/main.js
+++ API/main.js
@@ -58,12 +58,12 @@ app.get('/chores/:id', async (req, res) => {
  app.put('/chores/:id', async (req, res) => {
     try{
     let id = req.params.id
-    let {Title , Est_Time_min} = req.body
-    if(Title !== null){await pool.query("update chores set Title = $1 where id = $2", [Title,id])}
-    if(Est_Time_min !== null){await pool.query("update chores set Title = $1 where id = $2", [Est_Time_min,id])}
+    let {title , est_time_min} = req.body
+    if(title !== null){await pool.query("update chores set Title = $1 where id = $2", [title,id])}
+    if(est_time_min !== null){await pool.query("update chores set Title = $1 where id = $2", [est_time_min,id])}
     let data = await pool.query("select * from chores where id = $1", [id]) 
     console.log(data.rows)
-     res.status(200).send({Title})
+     res.status(200).send({title})
               
 } catch (error) {
     res.status(500).send(error)
