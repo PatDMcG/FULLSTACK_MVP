88ff9716cdf9e2b7a859ca781b60009196baf09b
PatDMcG <110838464+PatDMcG@users.noreply.github.com>
2023-06-21 10:27:39 -0400

    MVP completion pushing formatted code

--- API/main.js
+++ API/main.js
@@ -1,132 +1,130 @@
-const {Pool} = require('pg')  
-const express = require('express')
-const dotenv = require('dotenv')
-const path = require('path')
-dotenv.config()
-const app = express()
-app.use(express.json())
-app.use("/" , express.static(path.join(__dirname, '../Front_END')))
-const PORT = process.env.PORT
-const connectionString = process.env.DATABASE_URL
+const { Pool } = require("pg");
+const express = require("express");
+const dotenv = require("dotenv");
+const path = require("path");
+dotenv.config();
+const app = express();
+app.use(express.json());
+app.use("/", express.static(path.join(__dirname, "../Front_END")));
+const PORT = process.env.PORT;
+const connectionString = process.env.DATABASE_URL;
 const pool = new Pool({
-    connectionString,
-})
+  connectionString,
+});
 
 //get all
-app.get('/chores', async (req, res) => {
-    try {
-   
-   let data = await pool.query("select * from chores") 
-   console.log(data.rows)
-    res.status(200).send(data.rows)
-         
-} catch (error) {
-    res.status(500).send(error)
-        
-}
-})
+app.get("/chores", async (req, res) => {
+  try {
+    let data = await pool.query("select * from chores");
+    console.log(data.rows);
+    res.status(200).send(data.rows);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
 
 // get one
-app.get('/chores/:id', async (req, res) => {
-    try{
-    let id = req.params.id
-    let data = await pool.query("select * from chores where id = $1", [id]) 
-    console.log(data.rows)
-     res.status(200).send(data.rows)
-              
-} catch (error) {
-    res.status(500).send(error)
-        
-}
- })
- 
- // delete one
- app.delete('/chores/:id', async (req, res) => {
-    try{
-    let id = req.params.id
-    let data = await pool.query("delete from chores where id = $1",[id]) 
-    console.log(data.rows)
-     res.status(200).send(data.rows)
-              
-} catch (error) {
-    res.status(500).send(error)
-        
-}
- })
+app.get("/chores/:id", async (req, res) => {
+  try {
+    let id = req.params.id;
+    let data = await pool.query("select * from chores where id = $1", [id]);
+    console.log(data.rows);
+    res.status(200).send(data.rows);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
 
- //edit one
- app.put('/chores/:id', async (req, res) => {
-    try{
-    let id = req.params.id
-    let title = req.body.title
-    let est_time_min = req.body.est_time_min
-    if(title == undefined)
-    {
-        title = null
+// delete one
+app.delete("/chores/:id", async (req, res) => {
+  try {
+    let id = req.params.id;
+    let data = await pool.query("delete from chores where id = $1", [id]);
+    console.log(data.rows);
+    res.status(200).send(data.rows);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+
+//edit one
+app.put("/chores/:id", async (req, res) => {
+  try {
+    let id = req.params.id;
+    let title = req.body.title;
+    let est_time_min = req.body.est_time_min;
+    if (title == undefined) {
+      title = null;
     }
-    if(est_time_min == undefined)
-    {
-        est_time_min = null
+    if (est_time_min == undefined) {
+      est_time_min = null;
     }
-    console.log(est_time_min)
-    if(title !== null && title !== '' && title !== undefined)
-    {
-       await pool.query("update chores set Title = $1 where id = $2", [title,id])
+    console.log(est_time_min);
+    if (title !== null && title !== "" && title !== undefined) {
+      await pool.query("update chores set Title = $1 where id = $2", [
+        title,
+        id,
+      ]);
     }
-    if(est_time_min !== null && est_time_min !== '' && est_time_min !== undefined)
-    {
-       await pool.query("update chores set Est_Time_min = $1 where id = $2", [est_time_min,id])
+    if (
+      est_time_min !== null &&
+      est_time_min !== "" &&
+      est_time_min !== undefined
+    ) {
+      await pool.query("update chores set Est_Time_min = $1 where id = $2", [
+        est_time_min,
+        id,
+      ]);
     }
-    let data = await pool.query("select * from chores where id = $1", [id]) 
-    console.log(data.rows)
-     res.status(200).send({est_time_min})
-              
-} catch (error) {
-    res.status(500).send(error)
-        
-}
- })
+    let data = await pool.query("select * from chores where id = $1", [id]);
+    console.log(data.rows);
+    res.status(200).send({ est_time_min });
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
 
 //delete all
- app.delete('/chores', async (req, res) => {
-    try{
-    let data = await pool.query("delete from chores") 
-    console.log(data.rows)
-     res.status(200).send(data.rows)
-              
-} catch (error) {
-    res.status(500).send(error)
-        
-}
- })
+app.delete("/chores", async (req, res) => {
+  try {
+    let data = await pool.query("delete from chores");
+    console.log(data.rows);
+    res.status(200).send(data.rows);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
 
- // add one
- app.post('/chores', async (req, res) => {
-    try {
-        let title = req.body.title
-    let est_time_min = req.body.est_time_min
-    if(title == undefined)
-    {
-        title = null
+// add one
+app.post("/chores", async (req, res) => {
+  try {
+    let title = req.body.title;
+    let est_time_min = req.body.est_time_min;
+    if (title == undefined) {
+      title = null;
     }
-    if(est_time_min == undefined)
-    {
-        est_time_min = null
+    if (est_time_min == undefined) {
+      est_time_min = null;
     }
-    if(title !== null && title !== '' && est_time_min >= 0 && est_time_min !== null){let data = await pool.query("insert into chores (Title,Est_time_min) Values ($1,$2) returning *",[title,est_time_min])
-    console.log(data.rows)
-     res.status(200).send(data.rows)
+    if (
+      title !== null &&
+      title !== "" &&
+      est_time_min >= 0 &&
+      est_time_min !== null
+    ) {
+      let data = await pool.query(
+        "insert into chores (Title,Est_time_min) Values ($1,$2) returning *",
+        [title, est_time_min]
+      );
+      console.log(data.rows);
+      res.status(200).send(data.rows);
     }
-              
-    } catch (error) {
-        res.status(500).send(error.message)
-        console.log(error,error.message)
-        
-    }})
- 
+  } catch (error) {
+    res.status(500).send(error.message);
+    console.log(error, error.message);
+  }
+});
+
 app.listen(PORT, () => {
-    console.log(`listening on ${PORT}`)
-})
-
-
-
+  console.log(`listening on ${PORT}`);
+});
--- Front_END/main.js
+++ Front_END/main.js
@@ -1,239 +1,242 @@
-const CORSBYPASS = "https://corsproxy.io/?"
-const API = "https://fullstack-mvp.onrender.com/"
+const CORSBYPASS = "https://corsproxy.io/?";
+const API = "https://fullstack-mvp.onrender.com/";
 
 async function grabber(input) {
+  const response = await fetch(`${input}`);
+  const data = await response.json();
+  displayer(data);
+}
+grabber(`${API}chores`);
 
-    const response = await fetch(`${input}`);
-      const data = await response.json();
-    displayer(data)
-    
+function displayer(data) {
+  for (i = 0; i < data.length; i++) {
+    e = data[i].id;
+    tile = document.createElement("div");
+    titleTile = document.createElement("div");
+    timeTile = document.createElement("div");
+    tile.id = `${e}`;
+    title = document.createElement("p1");
+    time = document.createElement("p1");
+    delet = document.createElement("button");
+    delet.id = e;
+    delet.addEventListener("click", async (e) => {
+      let res = await fetch(`${API}chores/${e.target.id}`, {
+        method: "DELETE",
+      });
+      let data = await res.json();
+      console.log(data);
+      setTimeout(reload, 100);
+      return data;
+    });
+    editTitle = document.createElement("button");
+    editTitle.id = e;
+    editTitle.addEventListener("click", (e) => {
+      let id = e.target.id;
+      edit(id);
+    });
+    editTime = document.createElement("button");
+    editTime.id = e;
+    editTime.addEventListener("click", (e) => {
+      let id = e.target.id;
+      editT(id);
+    });
+    editTitle.innerText = "Edit Name ";
+    editTime.innerText = "Edit Time ";
+    delet.innerText = " Remove Chore ";
+    titleTile.appendChild(editTitle);
+    timeTile.appendChild(editTime);
+    title.innerText = data[i].title;
+    time.innerText = timeManipulation(data[i].est_time_min);
+    console.log(time);
+    titleTile.appendChild(title);
+    timeTile.appendChild(time);
+    tile.className = "test";
+    titleTile.className = "inner";
+    timeTile.className = "inner";
+    tile.appendChild(titleTile);
+    tile.appendChild(timeTile);
+    tile.appendChild(delet);
+    document.getElementById("chores").appendChild(tile);
+  }
+}
+
+function timeManipulation(time) {
+  let hours = Math.floor(time / 60);
+  let minutes = time % 60;
+  let timeString = `${hours} hours and ${minutes} minutes`;
+  return timeString;
+}
+
+function edit(id) {
+  console.log(id);
+  document.getElementsByTagName("button")[0].style.display = "none";
+  document.getElementsByClassName("line_3")[0].style.display = "none";
+  document.getElementsByClassName("line_6")[0].style.display = "none";
+  while (document.getElementById("chores").lastChild) {
+    document
+      .getElementById("chores")
+      .removeChild(document.getElementById("chores").firstChild);
+  }
+  grabber(`${CORSBYPASS}${API}chores/${id}`);
+  let UI = document.createElement("input");
+  UI.type = "text";
+  UI.id = "UI";
+  p1 = document.createElement("p1");
+  p1.innerText = "INSERT NEW CHORE NAME";
+  p1.id = "KILL";
+  button = document.createElement("button");
+  button.innerText = "submit";
+  button.id = id;
+  document.getElementsByClassName("container_verticle")[0].appendChild(button);
+  document.getElementsByClassName("container_verticle")[0].appendChild(UI);
+  document.getElementsByClassName("container_verticle")[0].appendChild(p1);
+  button.addEventListener("click", async (e) => {
+    let input = document.getElementById("UI").value;
+    const requestOptions = {
+      method: "PUT",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ title: `${input}` }),
     };
-grabber(`${API}chores`)
-
-    function displayer(data)
-    {
-        for(i=0;i<data.length;i++)
-    {
-        e= data[i].id
-        tile = document.createElement("div")
-        titleTile = document.createElement("div")
-        timeTile = document.createElement("div")
-        tile.id = `${e}`
-        title = document.createElement("p1")
-        time = document.createElement("p1")
-        delet = document.createElement("button")
-        delet.id = e
-        delet.addEventListener("click", async (e) => 
-        {
-            let res = await fetch(`${API}chores/${e.target.id}`,{ method: 'DELETE'})
-                let data = await res.json()
-                console.log(data)
-                setTimeout(reload, 100)
-                return data
-                
-        })
-        editTitle = document.createElement("button")
-        editTitle.id = e
-        editTitle.addEventListener("click" , (e) => 
-        {
-            let id = e.target.id
-            edit(id)
-        })
-        editTime = document.createElement("button")
-        editTime.id = e
-        editTime.addEventListener("click" , (e) => 
-        {
-            let id = e.target.id
-            editT(id)
-        })
-        editTitle.innerText = "Edit Name "
-        editTime.innerText = "Edit Time "
-        delet.innerText = " Remove Chore "
-        titleTile.appendChild(editTitle)
-        timeTile.appendChild(editTime)
-        title.innerText = data[i].title
-        time.innerText = timeManipulation(data[i].est_time_min)
-        console.log(time)
-        titleTile.appendChild(title)
-        timeTile.appendChild(time)
-        tile.className = "test"
-        titleTile.className = "inner"
-        timeTile.className = "inner"
-        tile.appendChild(titleTile)
-        tile.appendChild(timeTile)
-        tile.appendChild(delet)
-        document.getElementById("chores").appendChild(tile)
-    }
-    }
-
-function timeManipulation(time)
-    {
-        let hours = Math.floor(time / 60)
-        let minutes = time % 60
-        let timeString = `${hours} hours and ${minutes} minutes`
-        return timeString
-    }
-
-    function edit(id)
-    {
-        console.log(id)
-        document.getElementsByTagName("button")[0].style.display = "none"
-        document.getElementsByClassName("line_3")[0].style.display = "none"
-        document.getElementsByClassName("line_6")[0].style.display = "none"
-        while(document.getElementById("chores").lastChild)
-        {
-            document.getElementById("chores").removeChild(document.getElementById("chores").firstChild)
-        }
-        grabber(`${CORSBYPASS}${API}chores/${id}`)
-        let UI = document.createElement("input")
-        UI.type = "text"
-        UI.id = 'UI'
-        p1 = document.createElement("p1")
-        p1.innerText = "INSERT NEW CHORE NAME"
-        p1.id = 'KILL'
-        button = document.createElement("button")
-        button.innerText = "submit"
-        button.id = id
-        document.getElementsByClassName("container_verticle")[0].appendChild(button)
-        document.getElementsByClassName("container_verticle")[0].appendChild(UI)
-        document.getElementsByClassName("container_verticle")[0].appendChild(p1)
-        button.addEventListener("click", async (e) => {
-            let input = document.getElementById('UI').value
-            const requestOptions = {
-             method: 'PUT',
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify({ title: `${input}` })
-            };
-         let res = await fetch(`${API}chores/${e.target.id}`, requestOptions);
-         let data = await res.json()
-         reset()
-         return data
-        })
-    }  
-
-function editT(id)
-{
-    console.log(id)
-    document.getElementsByTagName("button")[0].style.display = "none"
-    document.getElementsByClassName("line_3")[0].style.display = "none"
-    document.getElementsByClassName("line_6")[0].style.display = "none"
-    while(document.getElementById("chores").lastChild)
-    {
-        document.getElementById("chores").removeChild(document.getElementById("chores").firstChild)
-    }
-    grabber(`${CORSBYPASS}${API}chores/${id}`)
-    let UI = document.createElement("input")
-    UI.type = "text"
-    UI.id = 'UI'
-    p1 = document.createElement("p2")
-    p1.innerText = "INSERT NEW ESTIMATED TIME IN MINUTES"
-    p1.id = 'KILL'
-    button = document.createElement("button")
-    button.innerText = "submit"
-    button.id = id
-    document.getElementsByClassName("container_verticle")[0].appendChild(button)
-    document.getElementsByClassName("container_verticle")[0].appendChild(UI)
-    document.getElementsByClassName("container_verticle")[0].appendChild(p1)
-    button.addEventListener("click", async (e) => {
-        let input = document.getElementById('UI').value
-        const requestOptions = {
-         method: 'PUT',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ est_time_min: `${input}` })
-        };
-     let res = await fetch(`${API}chores/${e.target.id}`, requestOptions);
-     let data = await res.json()
-    
-     reset()
-     return data
-     
-    })
-}  
-function reset()
-{   
-    document.getElementsByTagName("button")[0].style.display = "block"
-    document.getElementsByClassName("line_3")[0].style.display = "flex"
-    document.getElementsByClassName("line_6")[0].style.display = "flex"
-    document.getElementsByClassName("container_verticle")[0].removeChild(document.getElementById('KILL'))
-    document.getElementsByClassName("container_verticle")[0].removeChild(document.getElementById('UI'))
-    console.log(document.getElementsByClassName("container_verticle")[0].removeChild(document.getElementsByClassName("container_verticle")[0].lastChild))
-    while(document.getElementById("chores").lastChild)
-    {
-        document.getElementById("chores").removeChild(document.getElementById("chores").firstChild)
-    }
-    grabber(`${API}chores`)
+    let res = await fetch(`${API}chores/${e.target.id}`, requestOptions);
+    let data = await res.json();
+    reset();
+    return data;
+  });
 }
 
-function reload()
-{
-    while(document.getElementById("chores").lastChild)
-    {
-        document.getElementById("chores").removeChild(document.getElementById("chores").firstChild)
-    }
-    grabber(`${API}chores`)
+function editT(id) {
+  console.log(id);
+  document.getElementsByTagName("button")[0].style.display = "none";
+  document.getElementsByClassName("line_3")[0].style.display = "none";
+  document.getElementsByClassName("line_6")[0].style.display = "none";
+  while (document.getElementById("chores").lastChild) {
+    document
+      .getElementById("chores")
+      .removeChild(document.getElementById("chores").firstChild);
+  }
+  grabber(`${CORSBYPASS}${API}chores/${id}`);
+  let UI = document.createElement("input");
+  UI.type = "text";
+  UI.id = "UI";
+  p1 = document.createElement("p2");
+  p1.innerText = "INSERT NEW ESTIMATED TIME IN MINUTES";
+  p1.id = "KILL";
+  button = document.createElement("button");
+  button.innerText = "submit";
+  button.id = id;
+  document.getElementsByClassName("container_verticle")[0].appendChild(button);
+  document.getElementsByClassName("container_verticle")[0].appendChild(UI);
+  document.getElementsByClassName("container_verticle")[0].appendChild(p1);
+  button.addEventListener("click", async (e) => {
+    let input = document.getElementById("UI").value;
+    const requestOptions = {
+      method: "PUT",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ est_time_min: `${input}` }),
+    };
+    let res = await fetch(`${API}chores/${e.target.id}`, requestOptions);
+    let data = await res.json();
+
+    reset();
+    return data;
+  });
 }
-document.getElementsByClassName("reset")[0].addEventListener("click" , async () => {  
-   await fetch(`${API}chores`,{ method: 'DELETE'})
- return setTimeout(reload, 100)})
-document.getElementById("new").addEventListener("click" , makeNew)
+function reset() {
+  document.getElementsByTagName("button")[0].style.display = "block";
+  document.getElementsByClassName("line_3")[0].style.display = "flex";
+  document.getElementsByClassName("line_6")[0].style.display = "flex";
+  document
+    .getElementsByClassName("container_verticle")[0]
+    .removeChild(document.getElementById("KILL"));
+  document
+    .getElementsByClassName("container_verticle")[0]
+    .removeChild(document.getElementById("UI"));
+  console.log(
+    document
+      .getElementsByClassName("container_verticle")[0]
+      .removeChild(
+        document.getElementsByClassName("container_verticle")[0].lastChild
+      )
+  );
+  while (document.getElementById("chores").lastChild) {
+    document
+      .getElementById("chores")
+      .removeChild(document.getElementById("chores").firstChild);
+  }
+  grabber(`${API}chores`);
+}
 
-function makeNew()
-{
-    while(document.getElementById("chores").lastChild)
-    {
-        document.getElementById("chores").removeChild(document.getElementById("chores").firstChild)
-    }
-    document.getElementsByClassName("line_6")[0].style.display= "none"
-    document.getElementsByClassName("line_3")[0].style.display= "none"
-    let UI = document.createElement("input")
-    UI.type = "text"
-    UI.id = 'UI'
-    p1 = document.createElement("p2")
-    p1.innerText = "INSERT ESTIMATED TIME IN MINUTES"
-    p2 = document.createElement("p2")
-    p2.innerText = "INSERT CHORE NAME"
-    tile = document.createElement("div")
-    titleTile = document.createElement("div")
-    timeTile = document.createElement("div")
-    title = document.createElement("input")
-    title.type = "text"
-    title.id = "title"
-    time = document.createElement("input")
-    time.type = "text"
-    time.id = "time"
-    submit = document.createElement("button")
-    submit.id = 0
-    submit.addEventListener("click", async (e) => 
-    {
+function reload() {
+  while (document.getElementById("chores").lastChild) {
+    document
+      .getElementById("chores")
+      .removeChild(document.getElementById("chores").firstChild);
+  }
+  grabber(`${API}chores`);
+}
+document
+  .getElementsByClassName("reset")[0]
+  .addEventListener("click", async () => {
+    await fetch(`${API}chores`, { method: "DELETE" });
+    return setTimeout(reload, 100);
+  });
+document.getElementById("new").addEventListener("click", makeNew);
 
-        let timevalue = parseInt(document.getElementById("time").value)
-        let titlevalue = document.getElementById("title").value
-        const requestOptions = {
-            method: 'POST',
-           headers: { 'Content-Type': 'application/json' },
-           body: JSON.stringify({ est_time_min: `${timevalue}`, title : `${titlevalue}` })
-           };
-        let res = await fetch(`${API}chores`, requestOptions);
-        let data = await res.json()
-            console.log(data)
-            setTimeout(reload, 100)
-            document.getElementsByClassName("line_6")[0].style.display= "flex"
-            document.getElementsByClassName("line_3")[0].style.display= "flex"
-            return data
-            
-    })
-    titleTile.appendChild(p2)
-    timeTile.appendChild(p1)
-    titleTile.appendChild(title)
-    timeTile.appendChild(time)
-    tile.className = "test"
-    titleTile.className = "inner"
-    timeTile.className = "inner"
-    submit.innerText = "ADD NEW CHORE"
-    tile.appendChild(titleTile)
-    tile.appendChild(timeTile)
-    tile.appendChild(submit)
-    document.getElementById("chores").appendChild(tile)
-
-
+function makeNew() {
+  while (document.getElementById("chores").lastChild) {
+    document
+      .getElementById("chores")
+      .removeChild(document.getElementById("chores").firstChild);
+  }
+  document.getElementsByClassName("line_6")[0].style.display = "none";
+  document.getElementsByClassName("line_3")[0].style.display = "none";
+  let UI = document.createElement("input");
+  UI.type = "text";
+  UI.id = "UI";
+  p1 = document.createElement("p2");
+  p1.innerText = "INSERT ESTIMATED TIME IN MINUTES";
+  p2 = document.createElement("p2");
+  p2.innerText = "INSERT CHORE NAME";
+  tile = document.createElement("div");
+  titleTile = document.createElement("div");
+  timeTile = document.createElement("div");
+  title = document.createElement("input");
+  title.type = "text";
+  title.id = "title";
+  time = document.createElement("input");
+  time.type = "text";
+  time.id = "time";
+  submit = document.createElement("button");
+  submit.id = 0;
+  submit.addEventListener("click", async (e) => {
+    let timevalue = parseInt(document.getElementById("time").value);
+    let titlevalue = document.getElementById("title").value;
+    const requestOptions = {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        est_time_min: `${timevalue}`,
+        title: `${titlevalue}`,
+      }),
+    };
+    let res = await fetch(`${API}chores`, requestOptions);
+    let data = await res.json();
+    console.log(data);
+    setTimeout(reload, 100);
+    document.getElementsByClassName("line_6")[0].style.display = "flex";
+    document.getElementsByClassName("line_3")[0].style.display = "flex";
+    return data;
+  });
+  titleTile.appendChild(p2);
+  timeTile.appendChild(p1);
+  titleTile.appendChild(title);
+  timeTile.appendChild(time);
+  tile.className = "test";
+  titleTile.className = "inner";
+  timeTile.className = "inner";
+  submit.innerText = "ADD NEW CHORE";
+  tile.appendChild(titleTile);
+  tile.appendChild(timeTile);
+  tile.appendChild(submit);
+  document.getElementById("chores").appendChild(tile);
 }
